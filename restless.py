c163ba085db1386e977343787fab776aa34b9b32
Christopher Lycan <96343159+777388@users.noreply.github.com>
2024-03-18 11:49:46 -0700

    Add files via upload

---
+++ restless.py
@@ -0,0 +1,7 @@
+import rest_framework.exceptions
+rest_framework.exceptions.ApiException.__setattr__.__sizeof__.__init__ #'${}'
+rest_framework.exceptions.SkipError.__subclasshook__ #"${}"
+rest_framework.exceptions.SkipError.__init_subclass__ #"${}"
+rest_framework.exceptions.SkipError.__delattr__ #"${}"
+import trace
+trace.Trace()
